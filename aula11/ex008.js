77175107589ac8fe559b043f5836f447b5ddb3fe
Vinicios000 <149599697+Vinicios000@users.noreply.github.com>
2023-12-13 00:31:53 -0300

    novo

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(` Avelocidade do seu carro é ${vel} Km/h`)
+if (vel > 60){
+    console.log (' Você ultrapassou avelocidade permitida. MULTADO!')
+}
+console.log(' dirija sempre usando cinto de segurança.')
