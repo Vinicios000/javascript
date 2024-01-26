d1899be9174783913be5ccc243aeea19e692a3f3
Vinicios000 <149599697+Vinicios000@users.noreply.github.com>
2024-01-26 15:08:03 -0300

    hoje

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,23 +1,19 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `agora são ${hora} horas.`
+function calcular() {    
+    var num = document.getElementById('inum')
+    var tab = document.getElementById('itab')
+    
+    
+    if(num.value.length == 0){      
+       window.alert = ('Por favor, digite um número!')
 
-    if(hora >= 0 && hora < 12){
-    //bom dia!
-    img.src = 'foto-manha.jpg'
-    document.body.style.background = '#e2cd9f'
-} else if(hora >= 12 && hora < 18){
-    //boa tarde!
-    img.src = 'foto-tarde.jpg'
-    document.body.style.background = '#b9846f'
-} else{
-    //boa noite!
-    img.src = 'foto-noite.jpg'
-    document.body.style.background = '#515154'
+
+    } else{
+    var n1 = Number(num.value)
+    
+    // for(let c = 1; c <= 10; c++ ){
+    //     res.innerHTML = `${n1} * ${c} ` + n1*c
+    //     }
+
+    }
+
 }
-}
-
-
